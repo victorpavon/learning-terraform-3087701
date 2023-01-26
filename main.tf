b5bb031cdf45e594e6e95d1bf88ee308cd9a2187
Victor Pavon <44030756+victorpavon@users.noreply.github.com>
2023-01-26 13:16:19 -0500

    Security group try #3

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port    = 80
   to_port      = 80
   protocol     = "tcp"
-  cider_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
-  secirity_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -51,9 +51,9 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port    = 443
   to_port      = 443
   protocol     = "tcp"
-  cider_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
-  secirity_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -61,7 +61,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port    = 0
   to_port      = 0
   protocol     = "-1"
-  cider_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
-  secirity_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
