3beaf599ca515d09ab017177420f62f156ec8b32
Victor Pavon <44030756+victorpavon@users.noreply.github.com>
2023-01-27 09:06:38 -0500

    Create dev vpc try 2

--- main.tf
+++ main.tf
@@ -21,24 +21,24 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+  Terraform   = "true"
+  Environment = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-vpc_security_group_ids = [module.blog_sg.security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Learning Terraform"
+  Name = "Learning Terraform"
   }
 }
 
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "4.17.1"
 
   name = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
